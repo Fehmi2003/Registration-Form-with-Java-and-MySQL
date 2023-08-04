5b1b88b3a8e412094d8918e9c4eb4833a12d75ab
Fehmi Tahsin Demirkan <fehmitahsindemirkan@gmail.com>
2023-08-04 08:58:29 +0300

    db connect version 1.0

--- src/lolo.java
+++ src/lolo.java
@@ -11,35 +11,35 @@ public class lolo extends JDialog {
     private JTextField tfName;
     private JTextField tfEmail;
     private JTextField tfPhone;
-    private JTextField tfSube;
-    private JPasswordField tfSifre;
-    private JPasswordField tfSifre2;
-    private JButton btnKayit;
-    private JButton btnIptal;
-    private JPanel kayitSayfasi;
+    private JTextField tfAddress;
+    private JPasswordField pfPassword;
+    private JPasswordField pfConfirmPassword;
+    private JButton btnRegister;
+    private JButton btnCancel;
+    private JPanel registerPanel;
 
     public lolo(JFrame parent) {
         super(parent);
-        setTitle("Yeni hesap oluştur");
-
-        setContentPane(kayitSayfasi);
-        setMinimumSize(new Dimension(450,474));
+        setTitle("Yeni Hesap Oluştur");
+        setContentPane(registerPanel);
+        setMinimumSize(new Dimension(450, 474));
         setModal(true);
         setLocationRelativeTo(parent);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-        btnKayit.addActionListener(new ActionListener() {
+        btnRegister.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 registerUser();
             }
         });
-        btnIptal.addActionListener(new ActionListener() {
+        btnCancel.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 dispose();
             }
         });
+
         setVisible(true);
     }
 
@@ -47,87 +47,87 @@ public class lolo extends JDialog {
         String name = tfName.getText();
         String email = tfEmail.getText();
         String phone = tfPhone.getText();
-        String address = tfSube.getText();
-        String password= String.valueOf(tfSifre.getPassword());
-        String confirmPassword=String.valueOf(tfSifre2.getPassword());
+        String address = tfAddress.getText();
+        String password = String.valueOf(pfPassword.getPassword());
+        String confirmPassword = String.valueOf(pfConfirmPassword.getPassword());
 
         if (name.isEmpty() || email.isEmpty() || phone.isEmpty() || address.isEmpty() || password.isEmpty()) {
             JOptionPane.showMessageDialog(this,
-                    "Lütfen Tüm Kutucukları Doldurunuz ! ",
-                    "Tekrar Dene",
-                    JOptionPane.ERROR_MESSAGE);
-            return;
-        }
-        if (!password.equals(confirmPassword)) {
-            JOptionPane.showMessageDialog(this,
-                    "Şifreler birbirleriyle eşleşmedi ! ",
+                    "Lütfen tüm boşlukları doldurunuz",
                     "Tekrar Deneyiniz",
                     JOptionPane.ERROR_MESSAGE);
             return;
         }
-    user=addUserToDatabe(name,email,phone,address,password);
-    if (user!=null){
-        dispose();
-    }
-    else {
-        JOptionPane.showMessageDialog(this,
-                "Yeni Kullanıcı oluştururken hata oluştu.",
-                "Tekrar Deneyiniz.",JOptionPane.ERROR_MESSAGE);
-    }
-    }
-    public  User user;
-    private User addUserToDatabe(String name, String email, String phone,String address, String password){
-        User user   = null;
-        final String DB_URL="jdbc:mysql://localhost:3306/jdbdemo.register:";
-        final String USERNAME="root";
-        final String PASSWORD="";
 
-
-      try{
-        Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-        // Connected to database successfully...
-
-        Statement stmt = conn.createStatement();
-        String sql = "INSERT INTO users (name, email, phone, address, password) " +
-                "VALUES (?, ?, ?, ?, ?)";
-        PreparedStatement preparedStatement = conn.prepareStatement(sql);
-        preparedStatement.setString(1, name);
-        preparedStatement.setString(2, email);
-        preparedStatement.setString(3, phone);
-        preparedStatement.setString(4, address);
-        preparedStatement.setString(5, password);
-
-        //Insert row into the table
-        int addedRows = preparedStatement.executeUpdate();
-        if (addedRows > 0) {
-            user = new User();
-            user.name = name;
-            user.email = email;
-            user.phone = phone;
-            user.address = address;
-            user.password = password;
+        if (!password.equals(confirmPassword)) {
+            JOptionPane.showMessageDialog(this,
+                    "Şifreler birbirleriyle eşleşmiyor",
+                    "Tekrar Deneyiniz",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
         }
 
-        stmt.close();
-        conn.close();
+        user = addUserToDatabase(name, email, phone, address, password);
+        if (user != null) {
+            dispose();
+        }
+        else {
+            JOptionPane.showMessageDialog(this,
+                    "Yeni Kullanıcı Oluştururken Hata Oluştu",
+                    "Tekrar Deneyiniz",
+                    JOptionPane.ERROR_MESSAGE);
+        }
     }
-      catch(Exception e)
-    {
-        e.printStackTrace();
-    }
+
+    public User user;
+    private User addUserToDatabase(String name, String email, String phone, String address, String password) {
+        User user = null;
+        final String DB_URL = "jdbc:mysql://localhost/MyStore?serverTimezone=UTC";
+        final String USERNAME = "root";
+        final String PASSWORD = "";
+
+        try{
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            // Connected to database successfully...
+
+            Statement stmt = conn.createStatement();
+            String sql = "INSERT INTO users (name, email, phone, address, password) " +
+                    "VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, email);
+            preparedStatement.setString(3, phone);
+            preparedStatement.setString(4, address);
+            preparedStatement.setString(5, password);
+
+            //Insert row into the table
+            int addedRows = preparedStatement.executeUpdate();
+            if (addedRows > 0) {
+                user = new User();
+                user.name = name;
+                user.email = email;
+                user.phone = phone;
+                user.address = address;
+                user.password = password;
+            }
+
+            stmt.close();
+            conn.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
 
         return user;
-}
+    }
 
-
-    public static void main(String []args){
-        lolo myForm= new lolo(null);
-            User user =myForm.user;
-            if (user!=null){
-                System.out.println("Giriş Başarılı. Hoşgeldiniz "+user.name);
-            }
-            else{
-                System.out.println("Giriş İptal Edildi.");
-            }
+    public static void main(String[] args) {
+        lolo myForm = new lolo(null);
+        User user = myForm.user;
+        if (user != null) {
+            System.out.println("Başarılı giriş: " + user.name);
+        }
+        else {
+            System.out.println("Kayıt olma iptal edildi");
+        }
     }
 }
